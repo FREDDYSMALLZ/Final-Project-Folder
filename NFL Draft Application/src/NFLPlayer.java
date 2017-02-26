c59ee63b2001e4a7dc538830f74801a332cfd891
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-26 17:32:32 -0600

    Posting after updating the project
    Signed-off-by: Fredrick Ondieki <freddysmallz@gmail.com>

--- NFL Draft Application/src/NFLPlayer.java
+++ NFL Draft Application/src/NFLPlayer.java
@@ -2,83 +2,152 @@ import java.util.Arrays;
 public class NFLPlayer {
 
 	public static void main(String[] args) {
+		player player = new player();//creates a new player object
+		String name;//Displays the name of the player
+		int DateofBirth ;//Displays the date of birth of the player
+		int Age;//Displays the age of the player
+		double Height;//Displays the height of the player
+		double Weight;// Displays the weight of the player in pounds
+		int Yards ;// Displays the yards
+		double Ratings;// Defines the ratings of the player 
+		int Touchdowns; //Displays the number of touch downs of the player
+		int Interception ;
+		int Seasons;
 		
-		Player NFLPlayer = new Player();//creates a new player object
-	}
-
-}
-
-class Player{
-	byte profilepic;//Displays the profile picture of the player
-	String name;//Displays the name of the player
-	private char DateofBirthandAddress ; //Displays the Date of birth/address
-	private int Age ; //Displays the current age of the player
-	double Height ;//Displays the height of the player
-	double Weight ;// Displays the weight of the player in pounds
-	int Yards ;// Displays the yards
-	double Ratings ;// Defines the ratings of the player 
-	int Touchdowns ; //Displays the number of touch downs of the player
-	int INT ; //Number of interceptions when an opposite player takes away the ball
-	int Seasons; //Number of seasons played
-	
-
-public Player() {
-
-	      
 		
-	}
-		
-		public double getheight(){
-			return Height;
-					
 		}
+	
+	}
+	
+
+	class player{
 		
-		public double getWeight(){
-			return Weight;
-				
+		
+		String name;//Displays the name of the player
+		int  DateofBirth ; //Displays the Date of birth/address
+		int Age ; //Displays the current age of the player
+		double Height ;//Displays the height of the player
+		double Weight ;// Displays the weight of the player in pounds
+		int Yards ;// Displays the yards
+		double Ratings ;// Defines the ratings of the player 
+		int Touchdowns ; //Displays the number of touch downs of the player
+		int Interception ; //Number of interceptions when an opposite player takes away the ball
+		int Seasons; //Number of seasons played
+		
+
+	public player(String name, int DateofBirth, int Age, int weight, int Height, int Yards, 
+			double Ratings, int Touchdwns,int interception, int Seasons) {
+		setname(name);
+		setDateofBirth(DateofBirth);
+		setAge(Age);
+		setHeight(Height);
+		setWeight(Weight);
+		setYards(Yards);
+		setRatings(Ratings);
+		setTouchdowns(Touchdowns);
+		setInterception(Interception);
+		setSeasons(Seasons);
+
+		}
+	      public player() {
 	}
-		public int getYards(){
-			return Yards;
-			
-}
-	    public double getRatings(){
-	    	return Ratings;
-	    	
-	    }
-	     public int getINT(){
-	    	 return INT;
-	    	 
-	     }
-	     public int getTouchdowns(){
-	    	 return Touchdowns;
-	    	 
-	     }
-	     
-          public int getSeasons(){
-        	  return Seasons;
-        	  
-          }
-	
-
+		public void setname(String newname){
+	    	  name = newname;
+	      }
+	      public void setDateofBirth(int newDateofBirth){
+	    	  DateofBirth = newDateofBirth;
+	    	  
+	      }
+		  public void setAge(int newAge){
+			  Age = newAge;
+			  
+		  }
+	    	 public void setHeight(double newHeight){
+	    		 Height = newHeight;
+	    	 }
+	    	 public void setWeight(double newWeight){
+	    		 Weight = newWeight;
+	        	  
+	          }
+		      public void setYards(int newYards){
+		    	  Yards = newYards;
+		      }
+              public void setRatings(double newRatings){
+            	  Ratings = newRatings;
+              }
+              public void setTouchdowns(int newTouchdowns){
+            	  Touchdowns = newTouchdowns;
+              }
+              public void setInterception(int newInterception){
+            	  Interception = newInterception;
+              }
+              public void setSeasons(int newSeasons){
+            	  Seasons = newSeasons;
+              }
+              public  String getname(){
+                  return name;
+              }
+              public int getDateofBirth(){
+            	  return DateofBirth;
+              }
+              public int getAge(){
+            	  return Age;
+              }
+              public double getHeight(){
+            	  return Height;
+              }
+              public double getWeight(){
+            	  return Weight;
+              }
+              public int getYards(){
+            	  return Yards;
+              }
+              public double getRatings(){
+            	  return Ratings;
+              }
+              public int getTouchdowns(){
+            	  return Touchdowns;
+              }
+              public int getInterception(){
+            	  return Interception;
+              }
+              public int getSeasons(){
+            	  return Seasons;
+              }
+              
 	}
-class PlayerManager{
-	Player[] playerlist = new Player[10];// The list will contain ten players
-	byte[] profilepic = new byte [10];
-	String[] name = new String[10];
-	private char[] DateofBirthandAddress = new char [10]; 
-	private int[] Age = new int [10]; 
-	double[] Height = new double[10]; 
-	double[] Weight = new double [10]; 
-	int[] Yards = new int [10];
-	double[] Ratings = new double [10];
-	int[] Touchdowns = new int [10];
-	int[] INT = new int [10];
-	int[] seasons = new int [10];
+                  
+		
+	class PlayerManager{
+		player[] playerlist = new player[10];// The list will contain ten players
+		String[] name = new String[10];
+		int [] DateofBirth= new int[10]; 
+		int[] Age = new int [10]; 
+		double[] Height = new double[10]; 
+		double[] Weight = new double [10]; 
+		int[] Yards = new int [10];
+		double[] Ratings = new double [10];
+		int[] Touchdowns = new int [10];
+		int[] Interception = new int [10];
+		int[] seasons = new int [10];
+	
+
+	
+	
+              
+	
+	}
+	
+	
 	
 	
 
+	
+	
 
-}
+	
+
+	
 
 
 
