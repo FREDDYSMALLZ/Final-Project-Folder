6ae091d09b5d6d6107f65e3588ed70f263fdefb4
Fredrick Onduso Ondieki <fredieondieki@yahoo.com>
2017-03-19 22:52:32 -0500

    commiting after correcting a few errors on the code

---
+++ NFL Draft Application/src/Celebrator.java
@@ -0,0 +1,17 @@
+
+public interface Celebrator {
+	
+		//dancing to celebrate
+		//Sliding on the carpet to celebrate
+		//Hugging team players for victory
+		
+		public default void celebrate(){
+			
+		}
+		public void dancing();
+		public void sliding();
+		public void Hugging();
+		public void knockingHeads();
+
+	}
+
--- NFL Draft Application/src/NFLPlayer.java
+++ NFL Draft Application/src/NFLPlayer.java
@@ -1,18 +1,18 @@
 import java.util.Arrays;
-public class NFLPlayer {
+public abstract class NFLPlayer {
 
 	public static void main(String[] args) {
 		player player = new player();//creates a new player object
 		String name;//Displays the name of the player
-		int DateofBirth ;//Displays the date of birth of the player
-		int Age;//Displays the age of the player
-		double Height;//Displays the height of the player
-		double Weight;// Displays the weight of the player in pounds
-		int Yards ;// Displays the yards
-		double Ratings;// Defines the ratings of the player 
-		int Touchdowns; //Displays the number of touch downs of the player
-		int Interception ;
-		int Seasons;
+		int dateofBirth ;//Displays the date of birth of the player
+		int age;//Displays the age of the player
+		double height;//Displays the height of the player
+		double weight;// Displays the weight of the player in pounds
+		int yards ;// Displays the yards
+		double ratings;// Defines the ratings of the player 
+		int touchdowns; //Displays the number of touch downs of the player
+		int interception ;
+		int seasons;
 		
 		
 		}
@@ -24,95 +24,95 @@ public class NFLPlayer {
 		
 		
 		String name;//Displays the name of the player
-		int  DateofBirth ; //Displays the Date of birth/address
-		int Age ; //Displays the current age of the player
-		double Height ;//Displays the height of the player
-		double Weight ;// Displays the weight of the player in pounds
-		int Yards ;// Displays the yards
-		double Ratings ;// Defines the ratings of the player 
-		int Touchdowns ; //Displays the number of touch downs of the player
-		int Interception ; //Number of interceptions when an opposite player takes away the ball
-		int Seasons; //Number of seasons played
+		int  dateofBirth ; //Displays the Date of birth/address
+		int age ; //Displays the current age of the player
+		double height ;//Displays the height of the player
+		double weight ;// Displays the weight of the player in pounds
+		int yards ;// Displays the yards
+		double ratings ;// Defines the ratings of the player 
+		int touchdowns ; //Displays the number of touch downs of the player
+		int interception ; //Number of interceptions when an opposite player takes away the ball
+		int seasons; //Number of seasons played
 		
 
-	public player(String name, int DateofBirth, int Age, int weight, int Height, int Yards, 
-			double Ratings, int Touchdwns,int interception, int Seasons) {
+	public player(String name, int dateofBirth, int age, int weight, int height, int yards, 
+			double ratings, int touchdowns,int interception, int seasons) {
 		setname(name);
-		setDateofBirth(DateofBirth);
-		setAge(Age);
-		setHeight(Height);
-		setWeight(Weight);
-		setYards(Yards);
-		setRatings(Ratings);
-		setTouchdowns(Touchdowns);
-		setInterception(Interception);
-		setSeasons(Seasons);
+		setDateofBirth(dateofBirth);
+		setAge(age);
+		setHeight(height);
+		setWeight(weight);
+		setYards(yards);
+		setRatings(ratings);
+		setTouchdowns(touchdowns);
+		setInterception(interception);
+		setSeasons(seasons);
 
 		}
 	      public player() {
 	}
 		public void setname(String newname){
-	    	  name = newname;
+	    	  this.name = newname;
 	      }
 	      public void setDateofBirth(int newDateofBirth){
-	    	  DateofBirth = newDateofBirth;
+	    	  this.dateofBirth= newDateofBirth;
 	    	  
 	      }
 		  public void setAge(int newAge){
-			  Age = newAge;
+			  this.age = newAge;
 			  
 		  }
 	    	 public void setHeight(double newHeight){
-	    		 Height = newHeight;
+	    		 height = newHeight;
 	    	 }
 	    	 public void setWeight(double newWeight){
-	    		 Weight = newWeight;
+	    		 weight = newWeight;
 	        	  
 	          }
 		      public void setYards(int newYards){
-		    	  Yards = newYards;
+		    	  yards = newYards;
 		      }
               public void setRatings(double newRatings){
-            	  Ratings = newRatings;
+            	  ratings = newRatings;
               }
               public void setTouchdowns(int newTouchdowns){
-            	  Touchdowns = newTouchdowns;
+            	  touchdowns = newTouchdowns;
               }
               public void setInterception(int newInterception){
-            	  Interception = newInterception;
+            	  interception = newInterception;
               }
               public void setSeasons(int newSeasons){
-            	  Seasons = newSeasons;
+            	  seasons = newSeasons;
               }
               public  String getname(){
                   return name;
               }
               public int getDateofBirth(){
-            	  return DateofBirth;
+            	  return dateofBirth;
               }
               public int getAge(){
-            	  return Age;
+            	  return age;
               }
               public double getHeight(){
-            	  return Height;
+            	  return height;
               }
               public double getWeight(){
-            	  return Weight;
+            	  return weight;
               }
               public int getYards(){
-            	  return Yards;
+            	  return yards;
               }
               public double getRatings(){
-            	  return Ratings;
+            	  return ratings;
               }
               public int getTouchdowns(){
-            	  return Touchdowns;
+            	  return touchdowns;
               }
               public int getInterception(){
-            	  return Interception;
+            	  return interception;
               }
               public int getSeasons(){
-            	  return Seasons;
+            	  return seasons;
               }
               
 	}
@@ -121,159 +121,121 @@ public class NFLPlayer {
 	class PlayerManager{
 		player[] playerlist = new player[10];// The list will contain ten players
 		String[] name = new String[10];
-		int [] DateofBirth= new int[10]; 
-		int[] Age = new int [10]; 
-		double[] Height = new double[10]; 
-		double[] Weight = new double [10]; 
-		int[] Yards = new int [10];
-		double[] Ratings = new double [10];
-		int[] Touchdowns = new int [10];
-		int[] Interception = new int [10];
-		int[] seasons = new int [10];
+		int [] dateofBirth= new int[10]; 
+		int[] age = new int [10]; 
+		double[] height = new double[10]; 
+		double[] weight = new double [10]; 
+		int[] yards = new int [10];
+		double[] ratings = new double [10];
+		int[] touchdowns = new int [10];
+		int[] interception = new int [10];
+		int[] seasons = new int [10]; {
+	
+	
+		
+		 for (int i = 0; i < dateofBirth.length; i++){
+			 System.out.println(dateofBirth[i] + " ");
+		 }
+		 for (int i = 0; i < age.length; i++){
+			 System.out.println(age[i] + " ");
+		 }
+		 for (int i = 0; i < yards.length; i++){
+			 System.out.println(yards[i] + " ");
+		 }
+		 for (int i = 0; i < interception.length; i++){
+			 System.out.println(interception[i] + " ");
+		 }
+		 for (int i = 0; i < seasons.length; i++){
+			 System.out.println(seasons[i] + " ");
+		 }
+		 for (int i = 0; i < height.length; i++){
+			 System.out.println(height[i] + " ");
+		 }
+		 for (int i = 0; i < weight.length; i++){
+			 System.out.println(weight[i] + " ");
+
+		 
 	
 	}
-	class OffensivePlayer extends player{
-		public static int Touchdowns = 1;
-		  public static  int Yards = 2;
+	class offensivePlayer extends NFLPlayer{
+		public int touchdowns = 1;
+		  public  int yards = 2;
 		  
 		 
 		  @Override
 		  public String toString(){
-			  return "OffensivePlayer";
+			  return "offensivePlayer";
 		  }
 		  public void setYards( int newYads){
-	    	  Yards = newYads;
+	    	  yards = newYads;
 		  }
 		  public void setTouchdowns(int newTouchdowns){
-        	  Touchdowns = newTouchdowns;
+        	  touchdowns = newTouchdowns;
 		  }
 		  public int getYards(){
-        	  return Yards;
+        	  return yards;
 		  }
 		  public int getTouchdowns(){
-        	  return Touchdowns;
+        	  return touchdowns;
 		  }
 		 }
-	class DefensivePlayer extends player{
-		public int Interception = 1;
-		public  int Tackles = 2;
+	class defensivePlayer extends NFLPlayer{
+		public int interception = 1;
+		public  int tackles = 2;
 		
 		@Override
 		  public String toString(){
-			  return "DeffensivePlayer";
+			  return "deffensivePlayer";
 		  }
 		public void setInterception( int newInterception){
-	    	  Interception = newInterception;
+	    	  interception = newInterception;
 		  }
-		  public void setTackles(int newTackles){
-      	  Tackles = newTackles;
+		  public void settackles(int newTackles){
+      	  tackles = newTackles;
 		  }
-		  public int Interception(){
-      	  return Interception;
+		  public int interception(){
+      	  return interception;
 		  }
-		  public int getTackles(){
-      	  return Tackles;
+		  public int gettackles(){
+      	  return tackles;
       	  
 		  }
 			
 	}
-public interface Celebrator {
-	//dancing to celebrate
-	//Sliding on the carpet to celebrate
-	//Hugging team players for victory
-	
-	public default void celebrate(){
-		
-	}
-	public void dancing();
-	public void sliding();
-	public void Hugging();
-	public void knockingHeads();
-	public void Running();
-}
-public class OffensivePlayer implements Celebrator {
-
-	public OffensivePlayer() {
-		
-	}
-	
-	@Override
-	public void dancing() {
-		
-	}
-
-	@Override
-	public void sliding() {
-		
-	}
-
-	@Override
-	public void Hugging() {
-		
-	}
-
-	@Override
-	public void knockingHeads() {
+ 
 
 	}
 
-	@Override
-	public void Running() {
 
+class deffensivePlayer implements Celebrator {
+
+	public deffensivePlayer() {
 	}
 
-	public static void main(String[] args) {
-
-
-	}
-
-}
-public class DeffensivePlayer implements Celebrator {
-
-	public DeffensivePlayer() {
-	}
-
-	@Override
 	public void dancing() {
 
 	}
 
-	@Override
 	public void sliding() {
 
 	}
 
-	@Override
 	public void Hugging() {
 		
 	}
 
-	@Override
 	public void knockingHeads() {
 	
 	}
 
-	@Override
 	public void Running() {
 		
 	}
-
-	public static void main(String[] args) {
-	
-
+}
 	}
 
-}
 
-	
-	
-	
-	
 
-	
-	
-
-	
 
 	
 
